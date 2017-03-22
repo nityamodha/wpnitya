1b0f1765244338490437e06b4882b30b5f17504c
nityamodha <nityamodha97@gmail.com>
2017-03-22 12:27:11 +0530

    Update script.js

--- test2/script.js
+++ test2/script.js
@@ -3,27 +3,31 @@ document.addEventListener("DOMContentLoaded",
   function (event) {
     
     // Unobtrusive event binding
-    document.querySelector("button")
+    document.getElementById("submit")
       .addEventListener("click", function () {
         
         // Call server to get the name
         $ajaxUtils
           .sendGetRequest("name.json", 
             function (res) {
-              var message = 
-                res.firstName + " " + res.lastName
-              if (res.likesChineseFood) {
-                message += " likes Chinese food";
-              }
-              else {
-                message += " doesn't like Chinese food";
-              }
-              message += " and uses ";
-              message += res.numberOfDisplays + 1;
-              message += " displays for coding.";
+              var message =  document.getElementById("studentname").value;
+                
+              if (message === res.st1){
+                var question1 = res.q1;
+                document.getElementById("content")
+                .innerHTML = "<h2>"+ question1+"</h2>"
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>" + message + "</h2>";
+              }
+              else if(message === res.st2){
+                var question2 = res.q2;
+                document.getElementById("content")
+                .innerHTML = "<h2>"+question2+"</h2>"
+               
+              }
+              else{
+                document.getElementById("content")
+                .innerHTML =  "<h2>"+"Student not Found"+"</h2>"
+              }
             });
       });
   }
